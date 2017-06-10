44630b28c8585b6e0aee7577ead0de07062f6f20
Amit <amit.k03377@gmail.com>
2017-06-11 10:24:40 +1200

    footer html finished

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,83 @@
+sliderint = 1;
+sliderNext = 2;
+
+
+
 $(document).ready(function(){
 
-	$(".navbar__mobilemenu").on("click",function(){
-		$(".navbar__menu").slideToggle();
-	});
+	// $(".navbar__mobilemenu").on("click",function(){
+	// 	$(".navbar__menu").slideToggle();
+	// });
 
-
-	 $('.your-class').slick();
+	$(".slider #1").fadeIn(1100);
+	startSlider();
 
 });
+
+function startSlider(){
+
+	count = $(".slider > p").length;
+	 loop = setInterval(function(){
+
+	 	if(sliderNext>count){
+	 		sliderNext = 1;
+	 		sliderint =1 ;
+	 	}
+
+	 	$(".slider > p").fadeOut(1100);
+	 	$(".slider >p#"+sliderNext).fadeIn(1100);
+
+	 	sliderint = sliderNext;
+	 	sliderNext = sliderNext + 1;
+	 },3000);
+
+}
+
+$("#left-button").on("click",function(event){
+	event.preventDefault();
+
+	newSlide = sliderint - 1;
+	showSlide(newSlide); 
+	 // alert("hurray! How are you!");
+});
+
+
+$("#right-button").on("click",function(event){
+	event.preventDefault();
+
+	newSlide = sliderint + 1;
+	showSlide(newSlide); 
+	 // alert("hurray! I am good thanks");
+});
+
+function showSlide(id){
+
+	if(id>count){
+	 		id = 1;
+	 	}else if (id<1){
+	 		id = count ;
+	 	}
+
+	 	$(".slider > p").fadeOut(1100);
+	 	$(".slider >p#"+id).fadeIn(1100);
+
+	 	sliderint = id;
+	 	sliderNext = id + 1;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
