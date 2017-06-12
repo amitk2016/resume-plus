f6b00579ee33bb770c674ce08bf4dec4673392a2
Amit <amit.k03377@gmail.com>
2017-06-13 02:05:58 +1200

    testimonial slider finished

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,5 @@
-
+sliderInt = 1;
+sliderNext = 2;
 
 $(document).ready(function(){
 
@@ -10,8 +11,87 @@ $(document).ready(function(){
 function startSlider(){
 	count = $(".testimonial__text").length;
 	
+	// $(".slider #1").delay(1500).hide("slide",{direction:"left"},1000);
 	loop = setInterval(function(){
-		// $(".testimonial__text").show();
+
+		if (sliderNext > count ) {
+			sliderNext = 1;
+			sliderInt = 1;
+		}
+		$(".slider p").delay(400).fadeOut(1100);
+		$(".slider p#"+sliderNext).delay(400).fadeIn(1000);
+
+		sliderInt = sliderNext;
+		sliderNext = sliderNext +1 ;
 
 	},3000);
 }
+
+$("#left-button").on("click",function(event){
+	event.preventDefault();
+
+	newSlide = sliderInt - 1;
+	showSlide(newSlide); 
+	 // alert("hurray! How are you!");
+});
+
+$("#right-button").on("click",function(event){
+	event.preventDefault();
+
+	newSlide = sliderInt + 1;
+	showSlide(newSlide); 
+	// alert("hurray! I am good thanks");
+});
+
+function stopLoop(){
+	window.clearInterval(loop);
+}
+
+function showSlide(id){
+	stopLoop();
+	if (id > count ) {
+			id = 1;
+		}else if(id<1){
+			id = count;
+		}
+		$(".slider p").fadeOut(300);
+		$(".slider p#"+id).fadeIn(300);
+
+		sliderInt = id;
+		sliderNext = id +1 ;
+		startSlider();
+
+}
+
+$(".slider p").hover(
+	function(){
+		stopLoop();
+	},
+	function(){
+		startSlider();
+	}
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
