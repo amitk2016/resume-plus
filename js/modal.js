cb7921ca0c0954fc1411bbf03d532996d3c04d0f
Amit <amit.k03377@gmail.com>
2017-06-14 16:25:03 +1200

    modal finished in about section

---
+++ js/modal.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	var $team = $("#our-team");
+	var $modal = $(".aboutModal");
+	var $closeButton = $("#aboutClose");
+	
+	// clicking the our team button will open the modal 
+	$team.on('click',openModal);
+
+	// clicking the close button will close the modal 
+	$closeButton .on('click',closeModal);
+
+
+	// pushes escape key to close the modal 
+	$(document).keyup(keyPressHandler);
+
+	function openModal(){
+		$modal.addClass("aboutModal__visible");
+		return false;
+		// alert("hello");
+	};
+
+	function closeModal(){
+		$modal.removeClass("aboutModal__visible");
+	};
+
+	function keyPressHandler(e){
+		if (e.keyCode == 27 ) {
+			closeModal();
+		}
+	};
+
+});
+
+
+
+
+
+
